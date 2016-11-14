dd5a3c321e51e324670a9199f6820b8fe0878e4a
wenhaoquantil <wenhao.lin@quantil.com>
2016-11-13 23:38:25 -0800

    trivial change

--- webpack_dev.config.js
+++ webpack_dev.config.js
@@ -7,10 +7,7 @@ module.exports = {
     },
     module: {
         loaders: [
-            { test: /\.js?$/, loaders: ['babel'], exclude: /node_modules/ },
-            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
-            { test: /\.jsx$/, exclude: /node_modules/, loader: 'babel-loader'},
-
+            { test: /\.js?$/, exclude: /node_modules/, loader: 'babel-loader'},
             { test: /\.css$/, loader: "style!css" },
             { test: /\.scss$/, loader: "style-loader!css-loader?modules" },
             {test: /\.less/,loader: 'style-loader!css-loader!less-loader'}
